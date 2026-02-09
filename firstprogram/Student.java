7f14b137ac2447a4d077d9a14a1f9975ba1a3530
Nithya Sree Pagadi <99230040703@klu.ac.in>
2026-02-09 22:46:38 +0530

    update

--- firstprogram/Student.java
+++ firstprogram/Student.java
@@ -4,5 +4,6 @@ public class Student {
     public static void main(String[] args) {
         System.out.println("tharun");
         System.out.println("nithya");
+        System.out.println("rohith");
     }
 }
